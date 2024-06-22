2d1e84a2c7b73f712887a41734fdc9142bfc88c5
henrydall@softwarestrategies.io <henrydall@softwarestrategies.io>
2024-06-22 09:40:23 -0700

    The response objects need to handle noargs construction.

--- common/src/main/kotlin/io/softwarestrategies/projectk/common/data/ProjectsResponse.kt
+++ common/src/main/kotlin/io/softwarestrategies/projectk/common/data/ProjectsResponse.kt
@@ -3,4 +3,4 @@ package io.softwarestrategies.projectk.common.data
 import com.fasterxml.jackson.annotation.JsonInclude
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-data class ProjectsResponse(val projects: MutableList<ProjectResponse>)
+data class ProjectsResponse(val projects: MutableList<ProjectResponse> = mutableListOf())
